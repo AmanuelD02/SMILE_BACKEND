dcad84c6a10b02e76b6540dd69219aa8c688331a
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-18 15:24:20 +0300

    OTP verification

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
+import os
+import random
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+from twilio.rest import Client
+from dotenv import load_dotenv
+from users.models import Verification
+from datetime import datetime, timedelta
+
 # Create your views here.
+load_dotenv()
 
 
 class RegisterView(APIView):
@@ -9,30 +17,47 @@ class RegisterView(APIView):
         serializer = UserSerializer(request.data)
         serializer.is_valid()
         serializer.save()
-        
 
         return Response(serializer.data)
 
 
 class SendOTPView(APIView):
     def post(self, request):
-        account_sid = ""
-        auth_token = ""
+        account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+        auth_token = os.getenv('TWILIO_ACCOUNT_TOKEN')
+        phone_number = os.getenv('TWILIO_PHONE_NUMBER')
+
         client = Client(account_sid, auth_token)
-        send_to = request.data['number']
+        recipient_phone_number = request.data['phone_number']
 
-        otp  = generateOTP()
+        otp = generateOTP()
+        if(Verification(phone_number=recipient_phone_number) == None):
+            verification = Verification()
 
+            verification.code = otp
+            verification.phone_num = recipient_phone_number
+            verification.expiration_date = datetime.now() + timedelta(seconds=300)
 
-        body = f"Your PINCODE is {str(otp)}" 
-        message = client.messages.create(from_="", body=body, to = send_to)
+            verification.save()
+        else:
+            verification = Verification(phone_number=recipient_phone_number)
+            verification.code = otp
+            verification.expiration_date = datetime.now() + timedelta(seconds=300)
+
+            verification.save()
+
+        body = f"Your Smile Verification code is {str(otp)}"
+        message = client.messages.create(
+            from_=phone_number,
+            body=body,
+            to=recipient_phone_number
+        )
+
         if message.sid:
             return Response('ok')
         return Response("Error")
 
 
-
-
 def generateOTP():
-    import random
-    return random.randrange(10000,99999)
+
+    return random.randrange(100000, 999999)
