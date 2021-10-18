47c8f35f793ed23d609a92279ba2d37e8bf14df9
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-18 15:24:01 +0300

    OTP - Serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,15 @@
 from django.db.models import fields
 from rest_framework import serializers
-from .models import User
+from .models import User, Verification
+
+
+class OTPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Verification
+        fields = ['phone_num', 'code']
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'full_name','phone_num']
+        fields = ['id', 'full_name', 'phone_num']
