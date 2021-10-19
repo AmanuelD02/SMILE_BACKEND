b7f04c84b88af82c899222bef33913a08402a6e7
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-19 16:27:37 +0300

    UnauthorizedUser

--- users/serializers.py
+++ users/serializers.py
@@ -9,7 +9,15 @@ class OTPSerializer(serializers.ModelSerializer):
         fields = ['phone_num', 'code']
 
 
+class UnauthorizedUserSerializer(serializers.Serializer):
+    phone_num = serializers.CharField(max_length=15)
+    message = serializers.CharField(max_length=255)
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'full_name', 'phone_num']
+
+
+#serializer = UnauthorizedUserSerializer()
