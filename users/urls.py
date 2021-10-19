53a6bd7c16058dc83e396f7cdcec68a84acc53f9
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-19 16:28:01 +0300

    get_otp and verify_code

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import  RegisterView
+from .views import RegisterView, SendOTPView, AuthenticateOTPView
 
 urlpatterns = [
-    
-    path('register',RegisterView.as_view()),
+    path('authenticate/get_otp', SendOTPView.as_view()),
+    path('authenticate/verify_code', AuthenticateOTPView.as_view()),
+    path('register', RegisterView.as_view()),
 ]
