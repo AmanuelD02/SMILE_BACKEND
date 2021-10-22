9741fdff369f941d8334ee1baaebddd9a13b290a
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-22 08:20:59 +0300

    Review fix

--- review/models.py
+++ review/models.py
@@ -4,26 +4,30 @@ from users.models import User, Dentist
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
+
 class Review(models.Model):
-    dentist_id= models.ForeignKey(Dentist,on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    dentist_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="Dentist")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    rating = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    rating = models.IntegerField(default=1, validators=[
+                                 MaxValueValidator(5), MinValueValidator(1)])
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at= models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['dentist_id', 'user_id'], name='Review')
+            models.UniqueConstraint(
+                fields=['dentist_id', 'user_id'], name='Review')
         ]
 
 
 class ReviewLike(models.Model):
     review_id = models.ForeignKey(Review, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    
-    
+
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['review_id', 'user_id'], name='ReviewLike')
+            models.UniqueConstraint(
+                fields=['review_id', 'user_id'], name='ReviewLike')
         ]
--- review/serializers.py
+++ review/serializers.py
@@ -3,14 +3,14 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import Review, ReviewLike
 
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
-        models = Review
-        fields = ['dentist_id', 'user_id','content', 'rating']
-
+        model = Review
+        fields = ['dentist_id', 'user_id', 'content', 'rating']
 
 
 class ReviewLikeSerializer(serializers.ModelSerializer):
     class Meta:
-        models = ReviewLike
-        fields = ['review_id','user_id']
+        model = ReviewLike
+        fields = ['review_id', 'user_id']
