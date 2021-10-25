ada3dd2d2f29fd1fce9457378e619f45377fe8ef
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-25 15:06:43 +0300

    Appointment Chat model

--- appointment/models.py
+++ appointment/models.py
@@ -1,40 +1,50 @@
 from django.db import models
 
-from users.models import User, Dentist 
+from users.models import User, Dentist
 from treatment.models import Treatment
 # Create your models here.
 
 
 class Availability(models.Model):
-    dentist_id = models.ForeignKey(Dentist,on_delete=models.CASCADE)
+    dentist_id = models.ForeignKey(Dentist, on_delete=models.CASCADE)
     avaialble_at = models.DateTimeField()
 
 
-
 class PendingAppointment(models.Model):
-    dentist_id = models.ForeignKey(Dentist,on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    dentist_id = models.ForeignKey(Dentist, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     avaiable_at = models.DateTimeField()
-    treatment_id = models.ForeignKey(Treatment,on_delete=models.CASCADE)     
-
+    treatment_id = models.ForeignKey(Treatment, on_delete=models.CASCADE)
 
 
 class Appointment(models.Model):
-    dentist_id = models.ForeignKey(Dentist,on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    dentist_id = models.ForeignKey(Dentist, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     avaiable_at = models.DateTimeField()
-    treatment_id = models.ForeignKey(Treatment,on_delete=models.CASCADE)     
+    treatment_id = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+
+class AppointmentChat(models.Model):
+    PRE_APPOINTMENT = 'pre'
+    POST_APPOINTMENT = 'post'
+    STATUS = [(PRE_APPOINTMENT, PRE_APPOINTMENT),
+              (POST_APPOINTMENT, POST_APPOINTMENT)]
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    dentist_id = models.ForeignKey(Dentist, on_delete=models.CASCADE)
+    appointment_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS, default=PRE_APPOINTMENT)
+    expiration_date = models.DateTimeField()
 
 
 class AppointmentMessage(models.Model):
-    chat_id = models.ForeignKey(Appointment,on_delete=models.CASCADE,related_name='appointment_chat')
+    chat_id = models.ForeignKey(
+        Appointment, on_delete=models.CASCADE, related_name='appointment_chat')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    sender_id = models.ForeignKey(User,on_delete=models.CASCADE, related_name="appointment_sender")
-    reciever_id = models.ForeignKey(User,on_delete=models.CASCADE, related_name="appointment_reciever")
-
-
-
-
-
+    sender_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="appointment_sender")
+    reciever_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="appointment_reciever")
--- appointment/serializer.py
+++ appointment/serializer.py
@@ -4,13 +4,10 @@ from rest_framework import serializers
 from .models import Availability, PendingAppointment, AppointmentMessage
 
 
-
 class AvailabiltySerializer(serializers.ModelSerializer):
     class Meta:
         model = Availability
-        fields = ['dentist_id','avaialble_at']
-
-
+        fields = ['dentist_id', 'avaialble_at']
 
 
 class PendingAppointmentSerializer(serializers.ModelSerializer):
@@ -23,13 +20,12 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = PendingAppointment
         fields = ['dentist_id', 'user_id', 'avaiable_at', 'treatment_id']
-
-
+        read_only_fields = ['created_at']
 
 
 class AppointmentMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = AppointmentMessage
-        fields = ['chat_id', 'content','created_at', 'sender_id','reciever_id']
-        read_only_fields  = ['created_at']
-
+        fields = ['chat_id', 'content',
+                  'created_at', 'sender_id', 'reciever_id']
+        read_only_fields = ['created_at']
