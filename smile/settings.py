9b1c15574ecdf8cbd9ad0da48905858f42b0c274
Mastewal B <se.mastewal.belete@gmail.com>
2021-10-25 22:55:33 +0300

    Static

--- smile/settings.py
+++ smile/settings.py
@@ -138,7 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # location where django collect all static files
+# location where you will store your static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'smile/static')
+                    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
