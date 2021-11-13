31fb6e04f19201b05e95e534e3184158d163bee2
MastewalB <se.mastewal.belete@gmail.com>
2021-11-13 23:58:12 +0300

    payment

--- payment/url.py
+++ payment/url.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import WalletView, UpdateContact
+from .views import WalletView, UpdateContact, PerformPayment
 
 url_patterns = [
     path('wallet/', WalletView.as_view()),
-    path('update_contact/', UpdateContact.as_view())
+    path('update_contact/', UpdateContact.as_view()),
+    path('payment/', PerformPayment.as_view())
 ]
--- smile/asgi.py
+++ smile/asgi.py
@@ -11,9 +11,11 @@ from smile.wsgi import *
 from appointment.routing import websocket_appointment_urlpatterns
 from consultation.routing import websocket_consultation_urlpatterns
 from django.core.asgi import get_asgi_application
+from django.conf.urls import url
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from consultation.middleware.tokenAuthMiddleware import TokenAuthMiddleware
+from consultation.middleware.chatControllMiddleware import ChatControllMiddleware
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smile.settings')
 
@@ -23,6 +25,7 @@ application = ProtocolTypeRouter({
     "websocket":  # TokenAuthMiddleware(
     URLRouter(
         #
+        # [url ]
         websocket_consultation_urlpatterns + websocket_appointment_urlpatterns
     )
     # )
