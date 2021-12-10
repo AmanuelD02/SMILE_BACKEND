8d8af667f89c3ebf8ef5ce255798e5dbd7cdf046
Mastewal B <se.mastewal.belete@gmail.com>
2021-12-10 20:42:56 +0300

    Model Modification

--- appointment/models.py
+++ appointment/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime , timedelta
+from datetime import datetime, timedelta
 from django.utils import timezone
 from django.db import models
 from django.db.models.signals import post_save, pre_save
@@ -7,7 +7,6 @@ from notification.models import Notification as Notify
 from fcm_django.models import FCMDevice
 from firebase_admin.messaging import Message, Notification
 from payment.models import Wallet
-
 import decimal
 
 from users.models import User, Dentist
@@ -30,19 +29,18 @@ class PendingAppointment(models.Model):
     treatment_id = models.ForeignKey(Treatment, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
     def save(self, *args, **kwargs):
         user = self.user_id
         wallet = Wallet.objects.filter(id=user.id).first()
         treatment = self.treatment_id
-        c =  wallet.balance - treatment.price 
+        c = wallet.balance - treatment.price
         zero = decimal.Decimal(0)
-        r =  c.compare(zero)
+        r = c.compare(zero)
 
-        if c.compare(zero) ==1:
+        if c.compare(zero) == 1:
             return super().save()
         else:
-            return 
+            return
 
 
 class Appointment(models.Model):
@@ -57,11 +55,11 @@ class Appointment(models.Model):
         user = self.user_id
         wallet = Wallet.objects.filter(id=user.id).first()
         treatment = self.treatment_id
-        c =  wallet.balance - treatment.price 
+        c = wallet.balance - treatment.price
         zero = decimal.Decimal(0)
-        r =  c.compare(zero)
+        r = c.compare(zero)
 
-        if c.compare(zero) ==1:
+        if c.compare(zero) == 1:
             wallet.balance = c
             wallet.save()
             r = wallet.balance
@@ -69,6 +67,7 @@ class Appointment(models.Model):
             return super().save()
         else:
             raise ValueError("Low Balance")
+
 
 class AppointmentChat(models.Model):
     PRE_APPOINTMENT = 'pre'
@@ -83,8 +82,6 @@ class AppointmentChat(models.Model):
     status = models.CharField(
         max_length=100, choices=STATUS, default=PRE_APPOINTMENT)
     expiration_date = models.DateTimeField()
-
-
 
 
 class AppointmentMessage(models.Model):
@@ -114,8 +111,9 @@ def notify_users(sender, instance, **kwargs):
     notification.save()
 
     reciever_id = instance.user_id
-    FCMDevice.objects.filter(user_id = reciever_id).send_message(message=Message(notification=Notification(title=title, body=body)))
-        # FCMDevice.objects.all().send_message(message:Message(Notification(title="Appointment success",body="The Densitst dentist_id has approved ur appointment")))
+    FCMDevice.objects.filter(user_id=reciever_id).send_message(
+        message=Message(notification=Notification(title=title, body=body)))
+    # FCMDevice.objects.all().send_message(message:Message(Notification(title="Appointment success",body="The Densitst dentist_id has approved ur appointment")))
 
 
 @receiver(post_save, sender=Appointment)
@@ -123,8 +121,6 @@ def delete_pending_appointments(sender, instance, **kwargs):
     appointment_date = instance.available_at
     pending = PendingAppointment.objects.filter(available_at=appointment_date)
     pending.delete()
-
-
 
 
 @receiver(post_save, sender=Appointment)
@@ -135,5 +131,5 @@ def create_appointment_chat(sender, instance, **kwargs):
     chat.appointment_id = instance
     chat.created_at = timezone.now()
     a = timezone.now() + timedelta(days=1)
-    chat.expiration_date =  timezone.now() + timedelta(days=1)
+    chat.expiration_date = timezone.now() + timedelta(days=1)
     chat.save()
--- consultation/models.py
+++ consultation/models.py
@@ -2,6 +2,13 @@ import uuid
 from django.db import models
 from users.models import User, Dentist
 from treatment.models import Treatment
+from payment.models import Wallet
+import decimal
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from notification.models import Notification as Notify
+from fcm_django.models import FCMDevice
+from firebase_admin.messaging import Message, Notification
 # Create your models here.
 
 
@@ -16,6 +23,27 @@ class ConsultationRequest(models.Model):
         ]
 
 
+class PendingConsultation(models.Model):
+    id = models.AutoField(primary_key=True)
+    dentist_id = models.ForeignKey(Dentist, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        user = self.user_id
+        wallet = Wallet.objects.filter(id=user_id).first()
+        dentist = Dentist.objects.filter(id=dentist_id)
+        if dentist and dentist.consultation_availabilty:
+            price = wallet.balance - dentist.consultation_availabilty
+            zero = decimal.Decimal(0)
+            if c.compare(zero) == 1:
+                return super().save()
+            else:
+                return ValueError("Low Balance")
+        else:
+            raise ValueError("Dentist Not Available")
+
+
 class Consultation(models.Model):
 
     CONSULTATION_STATUS_OPEN = 'o'
@@ -28,9 +56,25 @@ class Consultation(models.Model):
     dentist_id = models.ForeignKey(Dentist, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, default=CONSULTATION_STATUS_OPEN)
-    starting_time = models.DateTimeField()
+    starting_time = models.DateTimeField(auto_now_add=True)
     ending_time = models.DateTimeField(blank=True)
-    treatment_id = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        user = self.user_id
+        wallet = Wallet.objects.filter(id=user.id).first()
+        treatment = self.treatment_id
+        c = wallet.balance - treatment.price
+        zero = decimal.Decimal(0)
+        r = c.compare(zero)
+
+        if c.compare(zero) == 1:
+            wallet.balance = c
+            wallet.save()
+            r = wallet.balance
+
+            return super().save()
+        else:
+            raise ValueError("Low Balance")
 
 
 class ConsultationMessage(models.Model):
@@ -47,15 +91,33 @@ class ConsultationMessage(models.Model):
         return ConsultationMessage.objects.order_by('-created_at').all()
 
 
-
 class ConsultationTask(models.Model):
     task_id = models.CharField(max_length=255)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    consultation_id = models.ForeignKey(Consultation,on_delete=models.CASCADE)
+    consultation_id = models.ForeignKey(Consultation, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['task_id', 'user_id','consultation_id'], name='ConsultationTask')
+                fields=['task_id', 'user_id', 'consultation_id'], name='ConsultationTask')
         ]
 
+
+@receiver(post_save, sender=Consultation)
+def notify_users(sender, instance, **kwargs):
+    dentist = Dentist.objects.filter(id=instance.dentist_id)
+
+    title = dentist.name
+    body = "Consultation Chat Approved! Start Messaging"
+
+    notification = Notify()
+    notification.type = 'consultation'
+    notification.content = body
+    notification.sender_id = instance.dentist_id.id
+    notification.reciever_id = instance.user_id
+    notification.created_at = datetime.now()
+
+    notification.save()
+    reciever_id = instance.user_id
+    FCMDevice.objects.filter(user_id=reciever_id).send_message(
+        message=Message(notification=Notification(title=title, body=body)))
