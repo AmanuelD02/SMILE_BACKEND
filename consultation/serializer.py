0e29a48d2965d847752d4e4bf6078692b320764c
Mastewal B <se.mastewal.belete@gmail.com>
2021-12-10 20:43:39 +0300

    serializer class

---
+++ consultation/serializer.py
@@ -0,0 +1,26 @@
+from django.db.models import fields
+from rest_framework import serializers
+from consultation.models import PendingConsultation, Consultation, ConsultationMessage
+from users.models import Dentist, User
+
+
+class PendingConsultationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PendingConsultation
+        fields = ['id', 'dentist_id', 'user_id']
+        read_only_fields = ['id']
+
+
+class ConsultationMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConsultationMessage
+        fields = ['id', 'chat_id', 'content',
+                  'created_at', 'sender_id', 'receiver_id']
+        read_only_fields = ['created_at', 'id']
+
+
+class ConsultationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Consultation
+        fields = ['id', 'dentist_id', 'user_id',
+                  'status', 'starting_time', 'ending_time']
