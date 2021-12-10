3813f85e415d7656e8100e75db4a4561fe656037
Mastewal B <se.mastewal.belete@gmail.com>
2021-12-10 20:43:25 +0300

    Paths

--- consultation/urls.py
+++ consultation/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from . import views
+from .views import PendingConsultationView, PendingConsultationDetailView, PendingConsultationListView, ApproveConsultationView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:consultation_chat_id>/',
-         views.consultation_chat, name='consultation_chat')
+         views.consultation_chat, name='consultation_chat'),
+
+    path('pending/', PendingConsultationView.as_view()),
+    path('pending/list', PendingConsultationListView.as_view()),
+    path('pending/<int:pending_id>', PendingConsultationDetailView.as_view()),
+    path('approve/<int:pending_id>/', ApproveConsultationView.as_view()),
+    # path('message/', ConsultationMessageView.as_view()),
+    # path('message/list/', GetConsultationMessageView.as_view()),
 ]
